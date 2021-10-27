2a5f11c8184789f47263e184dc9cb2917b4c3934
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2021-10-27 14:37:52 +0300

    Добавьте файлы проекта.

---
+++ Task8.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Log.txt";
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                int[] array = new int[10];
+                Random random = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(0, 10));
+                }
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                int[] intArrFile = File.ReadAllLines(path).Select(int.Parse).ToArray();
+                int sum = intArrFile.Sum();
+                Console.WriteLine(sr.ReadToEnd());
+                Console.WriteLine("Сумма чисел: {0}",sum);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\o.cheremikin\Desktop\РАБОТА\ПО\ОБУЧЕНИЕ\ЗАНЯТИЯ\Занятие 8\Files");
+            foreach (var item in dir.GetDirectories())
+            {
+                Console.WriteLine(item.Name);
+                foreach (var it in item.GetDirectories())
+                    Console.WriteLine(it.Name);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
